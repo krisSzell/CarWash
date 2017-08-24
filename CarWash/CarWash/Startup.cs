99f92b0cef9fa5d86c3ef3d408d5061cb14af1b7
Krzysztof Skorupa <skorupa.krzysztof1@gmail.com>
2017-08-24 15:58:58 +0200

    init

---
+++ CarWash/CarWash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarWash.Startup))]
+namespace CarWash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
