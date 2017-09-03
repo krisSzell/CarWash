92310bfca74ac5b126c30f8847b7ca99e8c0ceed
Krzysztof Skorupa <skorupa.krzysztof1@gmail.com>
2017-09-03 11:00:58 +0200

    added httpconfig to webapiconfig and registered app to use this config

--- CarWash/CarWash/App_Start/Startup.Auth.cs
+++ CarWash/CarWash/App_Start/Startup.Auth.cs
@@ -8,6 +8,7 @@ using Owin;
 using CarWash.ViewModels;
 using CarWash.Persistence.Models;
 using CarWash.Persistence;
+using System.Web.Http;
 
 namespace CarWash
 {
@@ -16,6 +17,11 @@ namespace CarWash
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            // Angular auth integration
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
             // Configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
